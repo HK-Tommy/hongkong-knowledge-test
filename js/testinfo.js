1f10b38416e67dae39e9b487cfa0f02f093450fc
HK 湯米Tommy <tlamhappy10@gmail.com>
2026-03-30 11:44:48 +0800

    Update

---
+++ js/testinfo.js
@@ -0,0 +1,55 @@
+export const historyQuestions = [{
+    EName: "history",
+    CName: "香港歷史篇",
+    TimeLimit: 30 * 60, // 30分鐘
+    dbName: "History"
+}];
+
+export const geoQuestions = [{
+    EName: "geo",
+    CName: "香港地理篇",
+    TimeLimit: 30 * 60, // 30分鐘
+    dbName: "Geography"
+}];
+
+export const trendsQuestions = [{
+    EName: "trends",
+    CName: "香港潮流文化篇",
+    TimeLimit: 30 * 60, // 30分鐘
+    dbName: "Trends"
+}];
+
+export const streetsQuestions = [{
+    EName: "streets",
+    CName: "香港街道名篇",
+    TimeLimit: 15 * 60, // 15分鐘
+    dbName: "Streets"
+}];
+
+export const TeaRestaurantSlangQuestions = [{
+    EName: "TeaRestaurantSlang",
+    CName: "香港茶餐廳術語篇",
+    TimeLimit: 15 * 60, // 15分鐘
+    dbName: "TeaRestaurantSlang"
+}];
+
+export const IntangibleCulturalHeritageQuestions = [{
+    EName: "IntangibleCulturalHeritage",
+    CName: "香港非物質文化遺產篇",
+    TimeLimit: 15 * 60, // 15分鐘
+    dbName: "IntangibleCulturalHeritage"
+}];
+
+export const ArtQuestions = [{
+    EName: "Art",
+    CName: "香港藝術篇",
+    TimeLimit: 15 * 60, // 15分鐘
+    dbName: "Art"
+}];
+
+export const BrandQuestions = [{
+    EName: "Brand",
+    CName: "香港品牌篇",
+    TimeLimit: 15 * 60, // 15分鐘
+    dbName: "Brand"
+}];
